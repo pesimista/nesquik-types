11de83dd2e603e5e5ee3784fd90efeb87934c3ff
MezzMar <25888252+MezzMar@users.noreply.github.com>
2022-04-15 06:27:21 +0000

    update: version v1.0.1

---
+++ dist/types/banner.type.d.ts
@@ -0,0 +1,33 @@
+import { Coordinates } from './coordinates.type';
+export declare type Banner = {
+    bannerID: string;
+    image: string;
+    largeImage?: string;
+    largeImagesArray?: string[];
+    type: 'lead' | 'category' | 'promo' | 'service';
+    name: string;
+    priority: number;
+    isHidden: boolean;
+    url: string;
+    queryParams?: BannerQueryParams;
+    disabled?: boolean;
+    caption?: string;
+    isDelivery?: boolean;
+    coordinates?: Coordinates;
+    radiusDistance?: number;
+    affiliateID?: string;
+    col?: string;
+    dateIn?: string;
+    dateOut?: string;
+    schedule?: string[];
+    gender?: 'male' | 'female';
+    ageRange?: string[];
+    backgroundColor?: string;
+    exclusiveFor: 'mobile' | 'desktop' | 'both';
+    gallery?: [];
+};
+declare type BannerQueryParams = {
+    storeid: string;
+    productID: string;
+};
+export {};
---
+++ dist/types/categories.type.d.ts
@@ -0,0 +1,23 @@
+export declare type CategoryObject<T> = {
+    ids: string[];
+    categoriesDescriptions: T[];
+};
+export declare type Category = {
+    categoryID: string;
+    parent: string;
+    image?: string;
+    name: string;
+    affiliateID: string;
+};
+export declare type MarketCategory = {
+    marketID: string;
+    order: number;
+    name: string;
+    categoryID: string;
+    image: string;
+    schedule: string[];
+    affiliateID: string;
+    banner?: string;
+    shape?: string;
+    parent?: string;
+};
